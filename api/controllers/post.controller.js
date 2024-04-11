ac5952081cd05900ddbff288667a0cc664331f7d
Tushar-rg <77859244+Tushar-rg@users.noreply.github.com>
2024-04-12 00:26:30 +0530

    add Property started

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -10,10 +10,19 @@ export const getPosts = async (req, res) => {
     }
 }
 export const getPost = async (req, res) => {
-    const id = req.params
+    const id = req.params.id
     try{
         const post = await prisma.post.findUnique({
-            where: {id}
+            where: {id},
+            include:{
+                postDetail:true,
+                user:{
+                    select:{
+                        username:true,
+                        avatar:true
+                    }
+                }
+            }
         })
         res.status(200).json(post)
     } catch (err) {
@@ -27,8 +36,11 @@ export const addPost = async (req, res) => {
     try{
         const newPost = await prisma.post.create({
             data: {
-                ...body,
-                userId: tokenUserId
+                ...body.postData,
+                userId: tokenUserId,
+                postDetail: {
+                    create: body.posDetail,
+                }
             }
         })
         res.status(200).json(newPost)
